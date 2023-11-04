9cea0be808246c2ea141c0ce816e61f6fef639e9
Hugo <duprez.hugo@gmail.com>
2023-11-04 09:30:14 +0100

    feat: portrait mode

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -1,5 +1,5 @@
-pub const GAME_RESOLUTION_X: usize = 80; // Change the game resolution for finer grains, ex: 160x120
-pub const GAME_RESOLUTION_Y: usize = 60;
-pub const WINDOW_SIZE: [f32; 2] = [640.0, 480.0];
+pub const GAME_RESOLUTION_X: usize = 45; // Change the game resolution for finer grains, ex: 160x120
+pub const GAME_RESOLUTION_Y: usize = 80;
+pub const WINDOW_SIZE: [f32; 2] = [360.0, 640.0];
 pub const TICK_RATE: u32 = 1; // Speed of the simulation, higher number is slower
 pub const BLOOD_SAFE_TIME: u32 = 200; // Stop blood interaction after this time to avoid infinite back and forth movement
